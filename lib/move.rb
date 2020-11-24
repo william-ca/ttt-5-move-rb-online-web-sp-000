2ee09c1a8e360eebee2b1d71d64e212c2490e021
Will Andre <wandre94@gmail.com>
2020-11-24 15:17:47 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,6 +11,6 @@ def input_to_index(user_input)
   user_input.to_i - 1
   end
 
-def move(array, index, value = "X")
-  array[index] = value
+def move(board, index, value = "X")
+  board[index] = value
 end
